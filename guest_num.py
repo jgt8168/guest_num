f4076343b4e675a924066df99fc31b1d998ef73d
jgt8168 <jgt8168@hotmail.com>
2024-06-15 15:48:32 +0800

    first version

---
+++ guest_num.py
@@ -0,0 +1,17 @@
+# 產生一個隨機整數1~100(不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出"終於猜對了!"
+# 猜錯的話 要告訴他 比答案大/小
+
+import random
+ans=random.randint(1,100)
+while True:
+    re=int(input("請輸入數字："))
+    if re==ans:
+        print("終於猜對了")
+        break
+    else:
+        if re< ans:
+            print("比答案小")
+        else:
+            print("比答案大")
